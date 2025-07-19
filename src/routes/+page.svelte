8cfef83e9f5ca8b7d2cb19d01918dd74d1955cb1
fallendamien <zamrirosli@gmail.com>
2025-07-19 22:36:37 +0800

    commit 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  let username = $state("");
+
+  $effect(() => {
+    console.log("$effect rune is initiated");
+
+    if (username) {
+      console.log(
+        `I will send the name to the database, current username is: ${username}`
+      );
+    }
+  });
+</script>
+
+<h1>Your username</h1>
+<input type="text" bind:value={username} />
+
+<p>{username}</p>
+
+<style>
+  h1 {
+    color: rgb(86, 86, 189);
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  /* .container {
+    display: flex;
+    justify-content: space-between;
+  } */
+</style>
