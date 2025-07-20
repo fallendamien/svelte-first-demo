fccf342a78a5a6f7e5762f89b7338fbca85df81b
fallendamien <11881550+fallendamien@users.noreply.github.com>
2025-07-20 20:23:59 +0800

    Done:SvelteKit Structure & Routing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import "../app.css";
+  let data = $props();
+</script>
+
+<header>Welcome! Landing Page</header>
+{@render data.children()}
---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  let data = $props();
+</script>
+
+<header>This is my blog header</header>
+{@render data.children()}
