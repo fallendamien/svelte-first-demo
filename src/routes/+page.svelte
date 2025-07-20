27941a7ffcf9239847a6e581b913609bdb1bcd1c
fallendamien <11881550+fallendamien@users.noreply.github.com>
2025-07-20 17:59:54 +0800

    Done added 'Edit mode'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
-<script>
-  import UserInput from "$lib/components/UserInput.svelte";
+<script lang="ts">
+  let userName = $state("Niklas");
 
-  let data = $props();
-  $inspect(data);
+  let isEditMode = $state(false);
+  let peopleWaiting = $state(["23fdk3232j", "12324dfdc33"]);
+  $inspect(peopleWaiting);
 </script>
 
-<UserInput userName={"Niklas"}>
-  <h1>This is the staff passed inside</h1>
-</UserInput>
+{#snippet userInput(exampleString: string)}
+  <h1>Your userName</h1>
+  {#if isEditMode}
+    <input type="text" bind:value={userName} />
+  {:else}
+    <p>{userName}</p>
+  {/if}
+{/snippet}
+
+{@render userInput("Example 1")}
+
+<button onclick={() => (isEditMode = !isEditMode)}>
+  {isEditMode ? "Save changes" : "Edit fields"}
+</button>
+
+<button onclick={() => peopleWaiting.push(new Date().getTime().toString())}
+  >I'm waiting too!</button
+>
