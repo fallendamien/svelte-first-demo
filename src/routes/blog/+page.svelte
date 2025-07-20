31c6e434e36fa77189a5d2efbcbc2b2f0b2b1782
fallendamien <11881550+fallendamien@users.noreply.github.com>
2025-07-21 00:17:51 +0800

    Done: Fetching data server & client side with load function

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,2 @@
 <h1>Welcome to our blog</h1>
+<a href="/blog/1">Our second blog article</a>
--- src/routes/blog/[articleId]/+page.ts
+++ src/routes/blog/[articleId]/+page.ts
@@ -1,7 +1,7 @@
 import { error } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import type { PageLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params }) => {
   console.log(params);
   const blogArticles = [
     {
