5d2715d1f8527237ae00a893a27d7c4c42526264
fallendamien <11881550+fallendamien@users.noreply.github.com>
2025-07-20 18:11:57 +0800

    Done added: #each iteration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,18 @@
 <script lang="ts">
-  let userName = $state("Niklas");
-
-  let isEditMode = $state(false);
   let peopleWaiting = $state(["23fdk3232j", "12324dfdc33"]);
   $inspect(peopleWaiting);
 </script>
 
-{#snippet userInput(exampleString: string)}
-  <h1>Your userName</h1>
-  {#if isEditMode}
-    <input type="text" bind:value={userName} />
-  {:else}
-    <p>{userName}</p>
-  {/if}
+<h1>Waiting queue</h1>
+{#snippet userInput(personId: string)}
+  <li>ID for person waiting: {personId}</li>
 {/snippet}
 
-{@render userInput("Example 1")}
-
-<button onclick={() => (isEditMode = !isEditMode)}>
-  {isEditMode ? "Save changes" : "Edit fields"}
-</button>
+<ul>
+  {#each peopleWaiting as person}
+    {@render userInput(person)}
+  {/each}
+</ul>
 
 <button onclick={() => peopleWaiting.push(new Date().getTime().toString())}
   >I'm waiting too!</button
